5f687fbbc3c66bcccb2295133b1aa93a327fe8f5
YoSoyLechuga97 <strongc97@gmail.com>
2024-03-12 16:04:00 -0600

    Finished personal scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -6,11 +6,41 @@ function Scoreboard() {
 
     //Obtain score JSON
     let personalScores = [];
-    const scoresText = localStorage.getItem('personalScores');
+    const scoresText = localStorage.getItem('playerScores');
     if (scoresText) {
-        scores = JSON.parse(scoresText);
+        personalScores = JSON.parse(scoresText);
     }
+    //Create player's personal scores table
+    const playerBodyEl = document.querySelector('#personal-scores');
 
+    if (personalScores.length) {
+        //Iterate over each row
+        for (const [i, score] of personalScores.entries()) {
+            //Create all table data elements
+            const placeTdEl = document.createElement('td');
+            const nameTdEl = document.createElement('td');
+            const scoreTdEl = document.createElement('td');
+
+            //Set place equal to the row index + 1
+            placeTdEl.textContent = i + 1;
+            //Set name equal to player's name
+            nameTdEl.textContent = score.name;
+            //Set score equal to player score
+            scoreTdEl.textContent = score.score;
+
+            //Create a row element to place all td's into
+            const currRowEl = document.createElement('tr');
+            //Add all td's to element
+            currRowEl.appendChild(placeTdEl);
+            currRowEl.appendChild(nameTdEl);
+            currRowEl.appendChild(scoreTdEl);
+
+            //Add finished row to the actual table
+            playerBodyEl.appendChild(currRowEl);
+        }
+    } else { //If cannot find any scores
+        playerBodyEl.innerHTML = '<tr><td colSpan=3>No scores yet!</tr>';
+    }
 }
 
 Scoreboard();
